83a910251104738c9b05745498d51313251100de
gershonv <gershonv@163.com>
2019-01-26 19:00:32 +0800

    Add 添加 md 编辑工具。

--- src/examples/markdown/write.jsx
+++ src/examples/markdown/write.jsx
@@ -1,8 +1,66 @@
 import React, { Component } from 'react'
 
+import hljs from 'highlight.js'
+import javascript from 'highlight.js/lib/languages/javascript'
+import 'highlight.js/styles/atelier-cave-dark.css'
+
+import marked from 'marked'
+
+import SimpleMDE from 'simplemde'
+import 'simplemde/dist/simplemde.min.css'
+
+hljs.registerLanguage('javascript', javascript)
+
+function translateMarkdown(plainText) {
+  return marked(plainText, {
+    renderer: new marked.Renderer(),
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: true,
+    smartLists: true,
+    smartypants: true,
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value
+    }
+  })
+}
+
 class Write extends Component {
+  state = { markdownText: '' }
+
+  componentDidMount() {
+    this.smde = new SimpleMDE({
+      element: document.getElementById('editor').childElementCount,
+      autofocus: true,
+      autosave: true,
+      previewRender: translateMarkdown
+    })
+  }
+
+  handleClick = () => {
+    console.log(this.smde.value())
+    const markdownText = translateMarkdown(this.smde.value())
+    console.log('markdown to html', markdownText)
+    this.setState({ markdownText })
+  }
+
   render() {
-    return <div>write</div>
+    return (
+      <div>
+        <h1>write</h1>
+        <button onClick={this.handleClick}>submit</button>
+        <textarea id="editor" />
+
+        {this.state.markdownText && (
+          <div
+            className="article-detail"
+            dangerouslySetInnerHTML={{ __html: translateMarkdown(this.state.markdownText) }}
+          />
+        )}
+      </div>
+    )
   }
 }
 
