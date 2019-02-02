8938eedc6cd79267bd0c0904b6e1c7b4ea092a8e
guosw <guoshaowei@pvmedtech.com>
2019-02-02 15:20:09 +0800

    sider 布局进行中

--- src/components/web/sider/index.jsx
+++ src/components/web/sider/index.jsx
@@ -1,9 +1,51 @@
 import React, { Component } from 'react'
 import './index.less'
+import { connect } from 'react-redux'
+import avatar from '@/assets/sider_avatar.png'
+import { Link } from 'react-router-dom'
 
+import { Divider, Tag } from 'antd'
+
+// colorList
+
+const colorList = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple']
+
+function random() {
+  const len = colorList.length
+  return Math.floor(Math.random() * len)
+}
+
+@connect(state => state.article)
 class BolgSider extends Component {
+  jumpTo = value => {
+    console.log(value)
+  }
+
   render() {
-    return <div className="main-menu" />
+    const { recentList, tags } = this.props
+    return (
+      <div className="sider-wrapper">
+        <img src={avatar} className="sider-avatar" alt="" />
+        <h2 className="name">郭大大</h2>
+        <div className="title">前端打杂人员，略微代码洁癖</div>
+        <Divider orientation="left">最近文章</Divider>
+        <ul className="recent-list">
+          {recentList.map(d => (
+            <li key={d.id}>
+              <Link to={`/${d.id}`}>{d.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <Divider orientation="left">标签</Divider>
+        <div className="tags-content">
+          {tags.map((tag, i) => (
+            <Tag key={i} color={colorList[i] ? colorList[i] : colorList[random()]}>
+              <Link to={tag}>{tag}</Link>
+            </Tag>
+          ))}
+        </div>
+      </div>
+    )
   }
 }
 
--- src/redux/article/reducer.js
+++ src/redux/article/reducer.js
@@ -1,7 +1,28 @@
 // state
 const defaultState = {
   categories: ['react', 'vue', 'javascript'],
-  tags: ['react', 'vue', 'javascript'],
+  tags: [
+    'react',
+    'vue',
+    'javascript',
+    'css',
+    'Dom',
+    'HTML',
+    'MySql',
+    'element-ui',
+    'sequelize',
+    '原型原型链',
+    '设计模式',
+    '跨域',
+    '面试系列'
+  ],
+  recentList: [
+    { id: 1, title: '如何利用vw+rem进行移动端布局' },
+    { id: 2, title: 'ES6、ES7、ES8特性一锅炖(ES6、ES7、ES8学习指南)' },
+    { id: 3, title: '随笔 - 如何用es6+写出优雅的js代码' },
+    { id: 4, title: 'react - PureComponent 和 memo' },
+    { id: 5, title: 'HTTP - 浏览器输入 url 后 http 请求返回的完整过程' }
+  ]
 }
 
 // reducer
