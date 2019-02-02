f9d58e8c5c7bf55708205e913b3d7f699928d295
guosw <guoshaowei@pvmedtech.com>
2019-02-02 14:05:21 +0800

    布局进行中...

--- src/components/web/layout/index.jsx
+++ src/components/web/layout/index.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import './index.less'
 import PropTypes from 'prop-types'
 
+import { Layout, Icon, Row, Col } from 'antd'
 import Header from '../header'
-import { Layout, Icon } from 'antd'
+import BolgSider from '../sider'
+import Navigation from '../navigation'
 
 const { Content, Footer, Sider } = Layout
 
@@ -13,18 +15,48 @@ class WebLayout extends Component {
   }
 
   render() {
+    // const responsiveLeft = { xxl: 4, xl: 5, lg: 5, sm: 4, xs: 24 }
+    // const responsiveRight = { xxl: 20, xl: 19, lg: 19, sm: 20, xs: 0 }
+    const siderLayout = { xxl: 4, xl: 5, lg: 5, sm: 0, xs: 0 }
+
+    const contentLayout = { xxl: 20, xl: 19, lg: 19, sm: 24, xs: 24 }
+    // const contentLayout = { xxl: 17, xl: 16, lg: 16, sm: 21, xs: 24 }
+    // const navigationLayout = { xxl: 3, xl: 3, lg: 3, sm: 3, xs: 0 }
     return (
-      <Layout>
-        <div className="app-container">
-          <Header />
-          <Layout>
-            <Content>{this.props.children}</Content>
-            <Sider theme="light">Sider</Sider>
-          </Layout>
-          <Footer style={{ textAlign: 'center' }}>
-            © 2019 <Icon type="user" /> Guodada
-          </Footer>
-        </div>
+      <Layout className="app-container">
+        <Header />
+        <Row className="main-wrapper">
+          <Col {...siderLayout}>
+            <BolgSider />
+          </Col>
+          <Col {...contentLayout}>
+            <Content>
+              {this.props.children}
+              <Navigation />
+              <Footer className="footer">
+                © 2019 <Icon type="user" /> Guodada
+              </Footer>
+            </Content>
+          </Col>
+        </Row>
+
+        {/* <Row className="main-wrapper">
+          <Col {...siderLayout}>
+            <BolgSider />
+          </Col>
+          <Col {...contentLayout}>
+            <Content>
+              {this.props.children}
+
+              <Footer style={{ textAlign: 'center' }}>
+                © 2019 <Icon type="user" /> Guodada
+              </Footer>
+            </Content>
+          </Col>
+          <Col {...navigationLayout}>
+            <Navigation />
+          </Col>
+        </Row> */}
       </Layout>
     )
   }
---
+++ src/components/web/sider/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './index.less'
+
+class BolgSider extends Component {
+  render() {
+    return <div className="main-menu" />
+  }
+}
+
+export default BolgSider
