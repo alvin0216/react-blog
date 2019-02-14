c6a569b3296ca902440a73a9f8218f506fce41df
gershonv <gershonv@163.com>
2019-02-15 02:59:36 +0800

    fix 评论框匹配显示问题

--- src/views/web/article/commentList.jsx
+++ src/views/web/article/commentList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, memo } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import blogAuthor from '@/assets/sider_avatar.png'
@@ -8,6 +8,56 @@ import { random, groupBy, translateMarkdown } from '@/lib'
 import { Comment, Avatar, Form, Button, List, Input, Icon } from 'antd'
 
 const { TextArea } = Input
+
+const CommentItem = ({
+  children,
+  item,
+  openReply,
+  fatherId,
+  levelOneId,
+  levelTwoId,
+  handleChange,
+  handleKeyUp,
+  onSubmit,
+  renderAvatar,
+  value
+}) => {
+  const level = item.replies ? 1 : 2
+  function handleClick(level) {
+    if (level === 1) openReply(level, item.id)
+    else openReply(level, item.id, fatherId)
+  }
+  return (
+    <Comment
+      actions={[<span onClick={() => handleClick(level)}>Reply to</span>]}
+      author={<span>{item.user && item.user.username}</span>}
+      avatar={renderAvatar(item)}
+      content={
+        <div
+          className="article-detail"
+          dangerouslySetInnerHTML={{ __html: translateMarkdown(item.content) }}
+        />
+      }>
+      {((level === 1 && levelOneId === item.id) || (level === 2 && levelTwoId === item.id)) && (
+        <div className="reply-form">
+          <TextArea
+            placeholder={`回复${item.user.username}...`}
+            value={value}
+            onChange={handleChange}
+            onKeyUp={handleKeyUp}
+          />
+          <div className="reply-form-controls">
+            <span className="tip">Ctrl or ⌘ + Enter</span>
+            <Button htmlType="submit" type="primary" disabled={!value.trim()} onClick={onSubmit}>
+              回复
+            </Button>
+          </div>
+        </div>
+      )}
+      {children}
+    </Comment>
+  )
+}
 
 @connect(state => ({
   userId: state.user.userId,
@@ -23,6 +73,8 @@ class CommentList extends Component {
     commentList: [],
     colorMap: {},
     commentId: 0,
+    levelOneId: 0, // 一级激活 id 用于判断评论框的显示
+    levelTwoId: 0, // 二级激活 id
     value: ''
   }
 
@@ -57,12 +109,15 @@ class CommentList extends Component {
     }
   }
 
-  openReply = commentId => {
-    this.setState({ commentId })
+  openReply = (level, id, commentId) => {
+    if (level === 1) {
+      this.setState({ commentId: id, levelTwoId: 0, levelOneId: id })
+    } else {
+      this.setState({ levelOneId: 0, levelTwoId: id, commentId })
+    }
   }
 
   handleChange = e => {
-    // console.log(e)
     this.setState({ value: e.target.value })
   }
 
@@ -83,19 +138,47 @@ class CommentList extends Component {
       })
       .then(res => {
         this.props.setCommentList(res.rows)
-        this.setState({ commentId: 0, value: '' })
+        this.setState({ commentId: 0, levelOneId: 0, levelTwoId: 0, value: '' })
       })
   }
 
   render() {
     const { commentList } = this.props
-    const { commentId, value } = this.state
+    const { levelOneId, value, levelTwoId } = this.state
     return (
       <div className="">
         {commentList.map(comment => (
+          <CommentItem
+            key={comment.id}
+            item={comment}
+            levelOneId={levelOneId}
+            value={value}
+            renderAvatar={this.renderAvatar}
+            openReply={this.openReply}
+            handleChange={this.handleChange}
+            handleKeyUp={this.handleKeyUp}
+            onSubmit={this.onSubmit}>
+            {comment.replies.map(reply => (
+              <CommentItem
+                key={reply.id}
+                item={reply}
+                levelTwoId={levelTwoId}
+                fatherId={comment.id}
+                value={value}
+                renderAvatar={this.renderAvatar}
+                openReply={this.openReply}
+                handleChange={this.handleChange}
+                handleKeyUp={this.handleKeyUp}
+                onSubmit={this.onSubmit}
+              />
+            ))}
+          </CommentItem>
+        ))}
+
+        {/* {commentList.map(comment => (
           <Comment
             key={comment.id}
-            actions={[<span onClick={() => this.openReply(comment.id)}>Reply to</span>]}
+            actions={[<span onClick={() => this.openReply(1, comment.id)}>Reply to</span>]}
             author={<span>{comment.user && comment.user.username}</span>}
             avatar={this.renderAvatar(comment)}
             content={
@@ -104,7 +187,7 @@ class CommentList extends Component {
                 dangerouslySetInnerHTML={{ __html: translateMarkdown(comment.content) }}
               />
             }>
-            {commentId === comment.id && (
+            {levelOneId === comment.id && (
               <div className="reply-form">
                 <TextArea
                   placeholder={`回复${comment.user.username}...`}
@@ -124,14 +207,34 @@ class CommentList extends Component {
             {comment.replies.map(reply => (
               <Comment
                 key={reply.id}
-                actions={[<span>Reply to</span>]}
+                actions={[<span onClick={() => this.openReply(2, reply.id, comment.id)}>Reply to</span>]}
                 author={<span>{reply.user && reply.user.username}</span>}
                 avatar={this.renderAvatar(reply)}
-                content={<p>{reply.content}</p>}
-              />
+                content={<p>{reply.content}</p>}>
+                {levelTwoId === reply.id && (
+                  <div className="reply-form">
+                    <TextArea
+                      placeholder={`回复${reply.user.username}...`}
+                      value={value}
+                      onChange={this.handleChange}
+                      onKeyUp={this.handleKeyUp}
+                    />
+                    <div className="reply-form-controls">
+                      <span className="tip">Ctrl or ⌘ + Enter</span>
+                      <Button
+                        htmlType="submit"
+                        type="primary"
+                        disabled={!value.trim()}
+                        onClick={this.onSubmit}>
+                        回复
+                      </Button>
+                    </div>
+                  </div>
+                )}
+              </Comment>
             ))}
           </Comment>
-        ))}
+        ))} */}
       </div>
     )
   }
