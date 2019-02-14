e5ca9316d7e0b468b432940fb98ed3b3e4abd3f4
guosw <guoshaowei@pvmedtech.com>
2019-02-14 18:24:19 +0800

    render avatar 方法

--- src/views/web/article/commentList.jsx
+++ src/views/web/article/commentList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import blogAuthor from '@/assets/sider_avatar.png'
 
 import { Comment, Avatar, Form, Button, List, Input, Icon } from 'antd'
 
@@ -14,8 +15,22 @@ class CommentList extends Component {
     commentList: []
   }
 
+  renderAvatar = comment => {
+    const { userId, avatarColor } = this.props
+    if (comment.userId === 1) return <Avatar src={blogAuthor} /> // userId = 1 博主~~~
+    if (comment.userId === userId) {
+      return (
+        <Avatar className="user-avatar" size="large" style={{ backgroundColor: avatarColor }}>
+          {comment.user.username}
+        </Avatar>
+      )
+    } else {
+      return <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    }
+  }
+
   render() {
-    const { commentList, userId, avatarColor } = this.props
+    const { commentList } = this.props
     return (
       <div className="">
         {commentList.map(comment => (
@@ -23,15 +38,7 @@ class CommentList extends Component {
             key={comment.id}
             actions={[<span>Reply to</span>]}
             author={<span>{comment.user.username}</span>}
-            avatar={
-              comment.userId === userId ? (
-                <Avatar className="user-avatar" size="large" style={{ backgroundColor: avatarColor }}>
-                  {comment.user.username}
-                </Avatar>
-              ) : (
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              )
-            }
+            avatar={this.renderAvatar(comment)}
             content={<p>{comment.content}</p>}>
             111
           </Comment>
