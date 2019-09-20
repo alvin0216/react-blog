33ce5afc61fb0856ea05f404fb9b4087cfffd9f1
guosw <guoshaowei@pvmedtech.com>
2019-09-20 11:54:22 +0800

    close: 关闭打包时自动启用依赖包分析

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -621,7 +621,7 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined
         }),
-      isEnvProduction && new BundleAnalyzerPlugin()
+      // isEnvProduction && new BundleAnalyzerPlugin()
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
