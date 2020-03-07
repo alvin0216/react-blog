6323c9248a9d6c8d7d9baf4cb8ae961c6ab8c138
guosw <gershonv@163.com>
2020-03-08 01:24:40 +0800

    fix: values ​​are not reset when switching inputs

--- src/components/Discuss/list.jsx
+++ src/components/Discuss/list.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
@@ -14,6 +14,10 @@ function CommentItem(props) {
   const { children, item, userInfo, articleId, commentId, replyId, replyVisible } = props
   const { user } = item
   const [value, setValue] = useState('')
+
+  useEffect(() => {
+    replyVisible && setValue('')
+  }, [replyVisible])
 
   function handleChange(e) {
     setValue(e.target.value)
