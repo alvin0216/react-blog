863e48d6b2286ccb480089990e41223ad72a8e46
alvin <alvin00216@163.com>
2020-07-09 10:09:55 +0800

    perf: lazy load components

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,29 +1,20 @@
 import { GITHUB } from '@/config'
 import Layout from '@/layout/web'
-import Home from '@/views/web/home'
-import Article from '@/views/web/article'
-import Archives from '@/views/web/archives'
-import Categories from '@/views/web/categories'
-import List from '@/views/web/tag'
-import About from '@/views/web/about'
-
 import lazy from '@/components/Lazy'
-const PageNotFound = lazy(() => import('@/components/404'))
-const GithubLogining = lazy(() => import('@/components/GithubLogining'))
 
 export default {
   path: '/',
   name: 'home',
   component: Layout,
   childRoutes: [
-    { path: '', component: Home },
-    { path: 'article/:id', component: Article },
-    { path: 'archives', component: Archives },
-    { path: 'categories', component: Categories },
-    { path: 'categories/:name', component: List },
-    { path: 'tags/:name', component: List },
-    { path: '/github', component: GITHUB.enable && GithubLogining },
-    { path: '/about', component: About },
-    { path: '*', component: PageNotFound }
-  ]
+    { path: '', component: lazy(() => import('@/views/web/home')) },
+    { path: 'article/:id', component: lazy(() => import('@/views/web/article')) },
+    { path: 'archives', component: lazy(() => import('@/views/web/archives')) },
+    { path: 'categories', component: lazy(() => import('@/views/web/categories')) },
+    { path: 'categories/:name', component: lazy(() => import('@/views/web/tag')) },
+    { path: 'tags/:name', component: lazy(() => import('@/views/web/tag')) },
+    { path: '/github', component: GITHUB.enable && lazy(() => import('@/components/GithubLogining')) },
+    { path: '/about', component: lazy(() => import('@/views/web/home')) },
+    { path: '*', component: lazy(() => import('@/components/404')) },
+  ],
 }
